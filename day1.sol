13484b4d4a281c6263ba0822d6042183aa6dfc27
Parshiv Kapoor <152784163+kapoor1309@users.noreply.github.com>
2025-12-26 00:06:58 +0530

    day1

---
+++ day1.sol
@@ -0,0 +1,74 @@
+pragma solidity >=0.5.0 <0.6.0;
+
+contract KittyInterface {
+    function getKitty(uint256 _id) external view returns (
+        bool isGestating,
+        bool isReady,
+        uint256 cooldownIndex,
+        uint256 nextActionAt,
+        uint256 siringWithId,
+        uint256 birthTime,
+        uint256 matronId,
+        uint256 sireId,
+        uint256 generation,
+        uint256 genes
+    );
+}
+
+contract ZombieGame {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) public ownerZombieCount;
+    mapping (uint => bool) public zombieHasEaten;
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
+    KittyInterface kittyContract = KittyInterface(ckAddress);
+
+    function _createZombie(string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+    function feedAndMultiply(uint _zombieId, uint _targetDna) internal {
+        require(zombieToOwner[_zombieId] == msg.sender);
+        require(zombieHasEaten[_zombieId] == false);
+
+        Zombie storage myZombie = zombies[_zombieId];
+
+        _targetDna = _targetDna % dnaModulus;
+        uint newDna = (myZombie.dna + _targetDna) / 2;
+
+        zombieHasEaten[_zombieId] = true;
+        _createZombie("NoName", newDna);
+    }
+
+    function feedOnKitty(uint _zombieId, uint _kittyId) public {
+        uint kittyDna;
+        (,,,,,,,,, kittyDna) = kittyContract.getKitty(_kittyId);
+        feedAndMultiply(_zombieId, kittyDna);
+    }
+}
